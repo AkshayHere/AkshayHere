37752453fb78374162c286198c41f2b59128038e
Dude <akshymohn@gmail.com>
2021-11-12 16:42:19 +0800

    test

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ require("isomorphic-unfetch");
 const { promises: fs } = require("fs");
 const path = require("path");
 
-const data = JSON.parse(fs.readFile(path.join(process.cwd(), "./hello.json")));
-
 async function main() {
     const readmeTemplate = (
         await fs.readFile(path.join(process.cwd(), "./CUSTOM_README.md"))
     ).toString("utf-8");
 
     // generate hello text
-    let helloTextArray = data;
-    console.log(helloTextArray);
+    const data = await fs.readFile(path.join(process.cwd(), "./hello.json"));
+    // let helloTextArray = data;
+    console.log(data);
     // let helloObject = helloTextArray.splice(Math.floor(Math.random()*helloTextArray.length), 1);
 
     const office_quote = await (
