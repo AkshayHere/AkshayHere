d6d882d176748505b4d2f500887fe8f76f845182
Dude <akshymohn@gmail.com>
2021-11-12 15:28:34 +0800

    Initial Commits

---
+++ index.js
@@ -0,0 +1,28 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function main() {
+    const readmeTemplate = (
+        await fs.readFile(path.join(process.cwd(), "./CUSTOM_README.md"))
+    ).toString("utf-8");
+
+    // generate hello text
+    let helloTextArray = path.join(process.cwd(), "./hello.json");
+    let helloObject = helloTextArray.splice(Math.floor(Math.random()*helloTextArray.length), 1);
+
+    const office_quote = await (
+        await fetch("https://officeapi.dev/api/quotes/random")
+    ).json();
+
+    console.log(office_quote);
+
+    const readme = readmeTemplate
+        .replace("{office_quote}", office_quote.data.content)
+        .replace("{office_character}", `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`)
+        .replace("{hello_text}", helloObject['hello'])
+
+    await fs.writeFile("README.md", readme);
+}
+
+main();
