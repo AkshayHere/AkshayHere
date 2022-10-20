6668c1209b2cd148135d5d65328a79db1abd896e
Akshay Mohan <akshay.mohan@sensiaglobal.com>
2022-10-20 12:52:29 +0800

    Fixed some stuff

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ async function main() {
     console.log(office_quote);
 
     if (office_quote) {
+        const currentDate = new Date().toLocaleString();
         const { data } = office_quote;
         const readme = readmeTemplate
             .replace("{office_quote}", data.content)
             .replace(
                 "{office_character}",
                 `- ${data.character.firstname} ${data.character.lastname}`
-            );
+            )
+            .replace("{currentDate}", currentDate);
         console.log(readme);
 
         await fs.writeFile("README.md", readme);
