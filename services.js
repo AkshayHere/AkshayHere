9b4638bd5f02bd6ecccba1d0f1c8923d80f1fa76
Akshay Mohan <akshay.mohan@sensiaglobal.com>
2023-02-27 14:34:13 +0800

    Fixing something

--- services.js
+++ services.js
@@ -1,7 +1,7 @@
-import fetch from "node-fetch";
+require("isomorphic-unfetch");
 
 // Fetches a random quote by different characters from the famous TV series `The Office` (US version)
-export const getRandomOfficeQuotes = async () => {
+async function getRandomOfficeQuotes() {
   const officeQuote = await fetch(
     "https://officeapi.dev/api/quotes/random"
   ).catch((err) => console.error(`Error : ${err.message}`));
@@ -11,4 +11,4 @@ export const getRandomOfficeQuotes = async () => {
 };
 
 // FOR TESTING
-// console.log(await getRandomOfficeQuotes());
+// console.log(getRandomOfficeQuotes());
